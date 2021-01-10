54452e0710eeb271866cc42d84a51bd55edd0a61
P <p@pop-os.localdomain>
2021-01-10 07:47:28 +0700

    initial setup

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Marker from "./Marker";
+
+const Board = () => {
+  return (
+    <div>
+      <Marker state="mark" />
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Marker.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+//state props -> marked, unmarked
+
+const Marker = (props) => {
+  return <div>Marker~{props.state}</div>;
+};
+
+export default Marker;
