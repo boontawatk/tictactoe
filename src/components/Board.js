2ae24aadd9da6d7a81db899303dfa0e5fce6986b
P <p@pop-os.localdomain>
2021-01-10 19:11:24 +0700

    add switch team event

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,40 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Marker from "./Marker";
 
 const Board = () => {
+  const [team, setTeam] = useState("false");
+  const teamSwitch = () => {
+    setTeam(!team);
+  };
   return (
-    <div>
+    <div onClick={teamSwitch}>
       <div className="row">
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
       </div>
       <div className="row">
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
       </div>
       <div className="row">
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
         <div className="col-sm p-0">
-          <Marker state="mark" />
+          <Marker state="mark" team={team} />
         </div>
       </div>
     </div>
--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./marker.css";
 // state - true,false
 
-const Marker = () => {
+const Marker = ({ team }) => {
   const [state, setState] = useState(false);
-  const teamIcon = "X";
+  const [teamIcon, setTeamIcon] = useState(" ");
   const toggle = () => {
     if (!state) setState(true);
   };
+  useEffect(() => {
+    if (state) {
+      setTeamIcon(team ? "X" : "O");
+    }
+  }, [state]);
   return (
     <div onClick={toggle}>
-      <div className="marker border p-0">{state ? teamIcon : " "}</div>
+      <div className="marker border p-0">{teamIcon}</div>
     </div>
   ); // replace o with space
 };
